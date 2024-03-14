811b847665c3111fa5dfbcc7405ae892e5e12b68
Md Humayun Rashid Khan <123394676+Humayun167@users.noreply.github.com>
2024-03-14 14:26:32 +0600

    2ND COMMIT

---
+++ false.js
@@ -0,0 +1,7 @@
+const person = {
+  name: "John",
+  age: 30,
+  gender: 'male'
+};
+
+console.log(person.hasOwnProperty("gender"));
--- oop.js
+++ oop.js
@@ -1,15 +1,14 @@
-
-class player{
-    constructor(name,age,skills){
-        this.name =name;
-        this.age = age;
-        this.skills=skills
-    }
-sayHello(){
-        return `i m ${this.name} - and i am ${this.skills}`
-    }
+class player {
+  constructor(name, age, skills) {
+    this.name = name;
+    this.age = age;
+    this.skills = skills;
+  }
+  sayHello() {
+    return `i m ${this.name} - and i am ${this.skills}`;
+  }
 }
 
-const masrafee =new player("Masrafee",33,"bowler");
+const masrafee = new player("Masrafee", 33, "bowler");
 
 console.log(masrafee.sayHello());
